343c0610a0a9e654768e43b89059291eb99de317
Samuel Poon <42005529+samuel-poon@users.noreply.github.com>
2025-02-26 11:33:08 -0500

    Rename to DEFAULT_MODEL

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='poshmarkfilter',
+    version='0.0.1',
+    description='Use AI to create custom filters on Poshmark',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/samuel-poon/poshmarkfilter-py',
+    python_requires='>=3.11',
+    install_requires=[
+        'requests',
+        'beautifulsoup4',
+        'openai'
+    ],
+    packages=['poshmarkfilter']
+)
