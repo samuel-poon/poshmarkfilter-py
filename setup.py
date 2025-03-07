b6c2c754ac835a5d43060124f24aaf398a00824d
Samuel Poon <42005529+samuel-poon@users.noreply.github.com>
2025-03-07 10:23:42 -0500

    Upgrade to version 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='poshmarkfilter',
-    version='0.0.1',
+    version='0.0.2',
     description='Use AI to create custom filters on Poshmark',
     long_description=long_description,
     long_description_content_type='text/markdown',
